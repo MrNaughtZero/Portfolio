487e6f8621bdf685f864a45482c9d73b18f65d98
Neil McNaught <hello@neilmcnaught.dev>
2026-07-28 20:26:55 +0100

    fix dates on blog

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -1,10 +1,7 @@
-const today = new Date();
-const formattedDate = `${today.getFullYear()} / ${String(today.getMonth() + 1).padStart(2, "0")} / ${String(today.getDate()).padStart(2, "0")}`;
-
 export const blogPosts = [
 	{
 		slug: "hey-im-neil",
-		date: formattedDate,
+		date: "2025 / 12 / 06",
 		title: "Hey, I'm Neil!",
 		category: "Introduction",
 		content: [
@@ -37,7 +34,7 @@ export const blogPosts = [
 	},
 	{
 		slug: "rebuilding-million-pound-ecommerce-store",
-		date: formattedDate,
+		date: "2026 / 02 / 04",
 		title: "Rebuilding a million-pound eCommerce store from the ground up",
 		category: "Project",
 		content: [
